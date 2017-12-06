c41cb11304c7a8bc8926aa53adc6a82bc1f841b7
YOGI <singhyogendra200@gmail.com>
2017-12-06 16:12:10 +0530

    contentprovider : searching using loader Api

--- contentprovider/src/main/java/com/example/contentprovider/MainActivity.java
+++ contentprovider/src/main/java/com/example/contentprovider/MainActivity.java
@@ -1,40 +1,45 @@
 package com.example.contentprovider;
 
 import android.app.LoaderManager;
-import android.content.ContentResolver;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
+    private EditText editText;
+    private String[] selectionArgs = {""};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+
     }
 
     public void fetchContacts(View view)
     {
         if (view.getId()==R.id.button)
         {
-            //initLoader() callback only creates a new loader if it does not exist
-            // otherwise it will just return earlier created loader and in that case
-            // the onLoadFinished method will get called directly
-            //but restartLoader() callback creates a new loader if it does not exist
-            // otherwise restarts the already existing loader by creating a new loader
-            //again and performs the load work and destroys the old loader then.
+            /*initLoader() callback only creates a new loader if it does not exist
+            otherwise it will just return earlier created loader and in that case
+            the onLoadFinished method will get called directly
+            but restartLoader() callback creates a new loader if it does not exist
+            otherwise restarts the already existing loader by creating a new loader
+            again and performs the load work and destroys the old loader then.*/
+
             getLoaderManager().restartLoader(1,null,this);
+
         }
 
         /*ArrayList<String> list = new ArrayList<>();
@@ -66,11 +71,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         if(id==1)
         {
+            selectionArgs[0] = editText.getText().toString();
             return new CursorLoader(this,
                     Uri.parse("content://com.example.yogi.syncapp.syncprovider"),
                     null,
-                    null,
-                    null,
+                    "name = ?",
+                    selectionArgs,
                     null);
         }
         return null;
@@ -86,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             int syncstatus = cursor.getInt(cursor.getColumnIndex("syncstatus"));
             String temp = "Name : "+name+"\nsyncstatus : "+syncstatus;
             list.add(temp);
-            Log.d("YOGI",temp);
+            /*Log.d("YOGI",temp);*/
 
         }
 
