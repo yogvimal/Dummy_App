cc44fbfb79fe7709bd59cb88110d932679ced224
YOGI <singhyogendra200@gmail.com>
2017-12-09 17:24:23 +0530

    contentprovider : marshmallow permission implementation

--- contentprovider/src/main/java/com/example/contentprovider/MainActivity.java
+++ contentprovider/src/main/java/com/example/contentprovider/MainActivity.java
@@ -1,107 +1,181 @@
 package com.example.contentprovider;
 
+import android.Manifest;
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, SearchView.OnQueryTextListener {
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 123;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+    private SimpleCursorAdapter cursorAdapter;
+    private String mfilter;
+    private ListView listView;
 
-    private EditText editText;
-    private String[] selectionArgs = {""};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS))
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setMessage("We need Contact permission")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.READ_CONTACTS},
+                                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                builder.show();
+
+           }
+           else
+            {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
+        else
+        {
+            initialCode();
+        }
 
     }
 
-    public void fetchContacts(View view)
+    void initialCode()
     {
-        if (view.getId()==R.id.button)
-        {
-            /*initLoader() callback only creates a new loader if it does not exist
-            otherwise it will just return earlier created loader and in that case
-            the onLoadFinished method will get called directly
-            but restartLoader() callback creates a new loader if it does not exist
-            otherwise restarts the already existing loader by creating a new loader
-            again and performs the load work and destroys the old loader then.*/
-
-            getLoaderManager().restartLoader(1,null,this);
-
-        }
-
-        /*ArrayList<String> list = new ArrayList<>();
-        //Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        Uri uri = Uri.parse("content://com.example.yogi.syncapp.syncprovider");
-        String[] projection = null;
-        String selection = null;
-        String[] selectionArgs = null;
-        String sortOrder = null;
-
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(uri,projection,selection,selectionArgs,sortOrder);
-/
-        while (cursor.moveToNext())
-        {
-            String name = cursor.getString(cursor.getColumnIndex("name"));
-            int syncstatus = cursor.getInt(cursor.getColumnIndex("syncstatus"));
-            String temp = "Name : "+name+"\nsyncstatus : "+syncstatus;
-            list.add(temp);
-            Log.d("YOGI",temp);
-
-        }
-
-        ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list));*/
+        cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2,
+                null,
+                new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER},
+                new int[]{android.R.id.text1,android.R.id.text2},0);
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(cursorAdapter);
+        getLoaderManager().initLoader(1,null,this);
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
+                if (grantResults.length>0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    initialCode();
+                }
+                return;
+        }
+    }
+
+    static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
+            ContactsContract.CommonDataKinds.Phone._ID,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_STATUS,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_PRESENCE,
+            ContactsContract.CommonDataKinds.Phone.PHOTO_ID,
+            ContactsContract.CommonDataKinds.Phone.LOOKUP_KEY,
+    };
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if(id==1)
+    public Loader<Cursor> onCreateLoader(int id, Bundle args)
+    {
+        Uri baseuri;
+        if(mfilter!=null)
         {
-            selectionArgs[0] = editText.getText().toString();
-            return new CursorLoader(this,
-                    Uri.parse("content://com.example.yogi.syncapp.syncprovider"),
-                    null,
-                    "name = ?",
-                    selectionArgs,
-                    null);
+            baseuri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI,
+                    Uri.encode(mfilter));
         }
-        return null;
+        else
+        {
+            baseuri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        }
+
+        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL)" +
+                " AND (" + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1)" +
+                " AND (" + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+
+        return new CursorLoader(this, baseuri,
+                CONTACTS_SUMMARY_PROJECTION, select, null,null);
+                /*ContactsContract.Contacts.DISPLAY_NAME +
+                        " COLLATE LOCALIZED ASC");*/
+
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
 
-        ArrayList<String> list = new ArrayList<>();
-        while (cursor.moveToNext())
-        {
-            String name = cursor.getString(cursor.getColumnIndex("name"));
-            int syncstatus = cursor.getInt(cursor.getColumnIndex("syncstatus"));
-            String temp = "Name : "+name+"\nsyncstatus : "+syncstatus;
-            list.add(temp);
-            /*Log.d("YOGI",temp);*/
-
-        }
-
-        ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list));
+        cursorAdapter.swapCursor(cursor);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+        cursorAdapter.swapCursor(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        super.onCreateOptionsMenu(menu);
+        MenuItem item = menu.add("search");
+        item.setIcon(android.R.drawable.ic_menu_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SearchView sv = new SearchView(this);
+        sv.setOnQueryTextListener(this);
+        item.setActionView(sv);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        mfilter = !TextUtils.isEmpty(newText) ? newText : null;
+        getLoaderManager().restartLoader(1,null,this);
+        return true;
     }
 }
